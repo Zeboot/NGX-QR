c5dc3b30f22ec6b0b535b562b01410bcddc91898
Zeboot <ctbuschmann@gmail.com>
2025-02-18 17:14:28 +0100

    opacity (#6)
    * fix generation context not refreshing
    
    * yarn setup
    
    * icon settings
    
    * yarn vers
    
    * fuck this
    
    * input formatting
    
    * layout settings
    
    * second layout
    
    * gap
    
    * add opacity settings

--- src/output/Label.tsx
+++ src/output/Label.tsx
@@ -6,7 +6,7 @@ import { LabelSettings } from "../settings/Layout";
 export default function Label({settings, value} : {settings: LabelSettings, value?: string}) {
     const QRContext = useQRSettings();
     return <div className={`${settings.width} ${settings.height} border-1 rounded print:border-0 flex justify-center items-center`}>
-            <div className={`${settings.inner_width || settings.width} ${settings.inner_height || settings.height} flex items-center ${settings.vertical ? "flex-col" : "flex-row"} mx-auto`}>
+            <div className={`${settings.inner_width || settings.width} ${settings.inner_height || settings.height} flex items-center ${settings.vertical ? "flex-col" : "flex-row"} mx-auto ${settings.gap}`}>
                 {value && <>
                     <QR value={value} settings={QRContext} size={settings.qr_size}/>
                     <Typography className={`flex-grow ${typeof(settings.text_size) === "function" ? settings.text_size(value) : settings.text_size}`}>{value}</Typography>
